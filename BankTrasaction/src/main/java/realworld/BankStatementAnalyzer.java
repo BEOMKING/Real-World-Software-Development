a62b3b33cf8ee8e635a1f4ab08bbce59d4bad52e
BEOMKING <qjawlsqjacks@naver.com>
2023-02-18 19:19:36 +0900

    Analyzer 클래스의 Parser 구현의 강결합 제거

---
+++ BankTrasaction/src/main/java/realworld/BankStatementAnalyzer.java
@@ -0,0 +1,29 @@
+package realworld;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.List;
+
+public class BankStatementAnalyzer {
+    public static final String RESOURCES = "src/main/resources/";
+
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+
+        collectSummary(bankStatementProcessor);
+    }
+
+    private void collectSummary(final BankStatementProcessor bankStatementProcessor) {
+        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total for transactions in January is " + bankStatementProcessor.selectInMonth(Month.JANUARY));
+        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+    }
+
+}
--- BankTrasaction/src/main/java/realworld/BankTransactionAnalyzer.java
+++ BankTrasaction/src/main/java/realworld/BankTransactionAnalyzer.java
@@ -1,30 +1,11 @@
 package realworld;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Month;
-import java.util.List;
 
 public class BankTransactionAnalyzer {
 
-    public static final String RESOURCES = "src/main/resources/";
-
     public static void main(String[] args) throws IOException {
-        final Path path = Paths.get(RESOURCES + args[0]);
-        final List<String> lines = Files.readAllLines(path);
-
-        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
-        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
-        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
-
-        collectSummary(bankStatementProcessor);
-    }
-
-    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
-        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
-        System.out.println("The total for transactions in January is " + bankStatementProcessor.selectInMonth(Month.JANUARY));
-        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
-    }
+        BankStatementAnalyzer bankStatementAnalyzer = new BankStatementAnalyzer();
+        bankStatementAnalyzer.analyze("file", new BankStatementCSVParser());
+    }ƒ
 }
