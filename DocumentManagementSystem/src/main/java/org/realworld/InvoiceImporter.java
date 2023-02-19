365eef6110825088a82a43c616af91bc37f4b164
BEOMKING <qjawlsqjacks@naver.com>
2023-02-19 23:00:25 +0900

    청구, 우편물, 보고서 Importer 구현

--- DocumentManagementSystem/src/main/java/org/realworld/InvoiceImporter.java
+++ DocumentManagementSystem/src/main/java/org/realworld/InvoiceImporter.java
@@ -4,25 +4,23 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 
-import static org.realworld.Attributes.ADDRESS;
-import static org.realworld.Attributes.BODY;
+import static org.realworld.Attributes.AMOUNT;
 import static org.realworld.Attributes.PATIENT;
 import static org.realworld.Attributes.TYPE;
 
-public class LetterImporter implements Importer {
+public class InvoiceImporter implements Importer {
     private static final String NAME_PREFIX = "Dear ";
+    private static final String AMOUNT_PREFIX = "Amount: ";
 
     @Override
     public Document importFile(final File file) throws IOException {
         final TextFile textFile = new TextFile(file);
 
         textFile.addLineSuffix(NAME_PREFIX, PATIENT);
-
-        final int lineNumber = textFile.addLines(2, String::isEmpty, ADDRESS);
-        textFile.addLines(lineNumber + 1, (line) -> line.startsWith("regards,"), BODY);
+        textFile.addLineSuffix(AMOUNT_PREFIX, AMOUNT);
 
         final Map<String, String> attributes = textFile.getAttributes();
-        attributes.put(TYPE, "LETTER");
+        attributes.put(TYPE, "INVOICE");
 
         return new Document(attributes);
     }
