8b4176c643c7547d3453209323eb1b4100b9ba1d
BEOMKING <qjawlsqjacks@naver.com>
2023-02-27 22:18:32 +0900

    User 구현, 로그인 기능 구현

--- Twitter/src/main/java/org/realworld/Twitter.java
+++ Twitter/src/main/java/org/realworld/Twitter.java
@@ -3,8 +3,18 @@ package org.realworld;
 import java.util.Optional;
 
 public class Twitter {
+    private final User users;
 
-    public Optional<SenderEndPoint> login(final Long id, final ReceiverEndPoint receiverEndPoint) {
-        return null;
+    public Twitter() {
+        this.users = new User();
+    }
+
+    public Optional<SenderEndPoint> login(final Long id, final String password, final ReceiverEndPoint receiverEndPoint) {
+        if (id == null || password == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(new SenderEndPoint() {
+        });
     }
 }
--- Twitter/src/test/java/org/realworld/TwitterTest.java
+++ Twitter/src/test/java/org/realworld/TwitterTest.java
@@ -2,21 +2,35 @@ package org.realworld;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
 public class TwitterTest {
+    private final ReceiverEndPoint receiverEndPoint = mock(ReceiverEndPoint.class);
 
     @Test
-    void 로그인_요청이_주어지면_로그인이_되어야_한다() {
+    void 로그인_요청이_주어지면_발신자_끝점을_리턴한다() {
+        // givenk
+        Twitter twitter = new Twitter();
+
+        // when
+        Optional<SenderEndPoint> senderEndPoint = twitter.login(1L, "good password", receiverEndPoint);
+
+        // then
+        assertThat(senderEndPoint).isPresent();
+    }
+
+    @Test
+    void 유효하지_않은_로그인_요청이_주어지면_빈_발신자_끝점을_리턴한다() {
         // given
         Twitter twitter = new Twitter();
 
         // when
-        SenderEndPoint senderEndPoint = twitter.login(1, new ReceiverEndPoint());
+        Optional<SenderEndPoint> senderEndPoint = twitter.login(null, "bad password", receiverEndPoint);
 
         // then
-    }
-
-    @Test
-    void 유효하지_않은_로그인_요청이_주어지면_로그인이_안되어야_한다() {
-
+        assertThat(senderEndPoint).isEmpty();
     }
 }
